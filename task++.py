541bb6f0bd05a93503ed8335862df0fd017a4b7a
task 1 <shahdkahlout8@gmail.com>
2023-08-05 00:03:47 +0300

    Task ++

---
+++ task++.py
@@ -0,0 +1,14 @@
+def square():
+    x = int(input("Enter number :"))
+    t = x * x
+    a = x * 4
+    print (t,a)
+square()
+def triangle ():
+    y = int(input("Enter len base: "))
+    z = int(input("Enter height: "))
+    a = int(input("Enter watar: "))
+    sp = 0.5*y*z
+    area = y+z+a
+    print(sp,area)
+triangle()
