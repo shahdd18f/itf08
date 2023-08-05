442923972d89bb9046606b0e8ab5dd1035011a61
task 1 <shahdkahlout8@gmail.com>
2023-08-05 04:36:13 +0300

    Task 3

---
+++ task3.py
@@ -0,0 +1,17 @@
+def add(number1,number2):
+    return number1+number2
+def multipulation(number1,number2):
+    return number1*number2
+def subtraction (number1,number2):
+    return number1-number2
+def division (number1,number2):
+    return number1/number2
+
+number1 = int(input("Enter number 1:"))
+number2 = int(input("Enter number 2:"))
+
+print(add(number1,number2))
+print(multipulation(number1,number2))
+print(subtraction (number1,number2))
+print(division (number1,number2))
+
