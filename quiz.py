08d5bbf04af3dfa0b94b41eeb4c3e1b97adb58d9
task 1 <shahdkahlout8@gmail.com>
2023-08-05 11:07:08 +0300

    ..

---
+++ quiz.py
@@ -0,0 +1,42 @@
+def circle ():
+    x = int(input("Enter number : "))
+    area =  x*x*3.14
+    Calculators = 2*3.14*x
+    print(area,Calculators)
+    if area >=10 :
+        print ("circle big")
+    elif area < 10 :
+
+        print ("circle small")
+    elif area <= 0 :
+        print ("error")
+circle()
+
+def triangle ():
+    y = int(input("Enter len base: "))
+    z = int(input("Enter height: "))
+    a = int(input("Enter watar: "))
+    area = 0.5*y*z
+    sp = y+z+a
+    print(sp,area)
+    if area >= 10:
+        print(" big")
+    elif area < 10:
+
+        print(" small")
+    elif area <= 0:
+        print("error")
+triangle()
+def square():
+    x = int(input("Enter number :"))
+    t = x * x
+    a = x * 4
+    print (t,a)
+    if a >= 10:
+        print(" big")
+    elif a < 10:
+
+        print(" small")
+    elif a <= 0:
+        print("error")
+square()
---
+++ test 1/__init__.py
@@ -0,0 +1,13 @@
+# def toupper(text):
+#     print(text.upper ())
+#
+# name = input("enter your name")
+# toupper()
+
+# def lower(x):
+#     name = x.lower()
+#     return name
+#
+# x= input("Enter your name :")
+# x = lower(x)
+# print(x)
