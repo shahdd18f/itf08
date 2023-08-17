4b4e5e4b44e079210d12e9bf207636878f75f441
task 1 <shahdkahlout8@gmail.com>
2023-08-17 16:06:22 +0300

    Task 5

---
+++ exam.py
@@ -0,0 +1,21 @@
+# qus 2
+
+my_list = [50,100,150,140]
+
+def list(my_list: list):
+     print(max(my_list) - min(my_list))
+
+list(my_list = [50,100,150,140])
+
+
+
+
+# qus 2
+def par (price ,member ):
+    if member == True :
+        price = price * 1/10
+        print(price)
+    else :
+        print(price)
+par(10)
+
